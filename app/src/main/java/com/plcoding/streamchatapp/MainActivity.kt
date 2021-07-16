883aa78cc7a1c08416bbfa7286150a23158f7d97
Philipp <mail@pl-coding.com>
2021-07-16 15:28:29 +0200

    Initial commit

---
+++ app/src/main/java/com/plcoding/streamchatapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.plcoding.streamchatapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.plcoding.streamchatapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/plcoding/streamchatapp/ui/BindingFragment.kt
@@ -0,0 +1,29 @@
+package com.plcoding.streamchatapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+abstract class BindingFragment<out T : ViewBinding> : Fragment() {
+
+    private var _binding: ViewBinding? = null
+    @Suppress("UNCHECKED_CAST")
+    protected val binding: T
+        get() = _binding as T
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = bindingInflater(inflater)
+        return _binding!!.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    protected abstract val bindingInflater: (LayoutInflater) -> ViewBinding
+}
